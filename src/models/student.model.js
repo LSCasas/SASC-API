6651d46d865a00c1faac4189e9b6c76c69eae875
Luis Casas <luisscasas45@gmail.com>
2025-04-01 16:39:39 -0600

    FEAT/ add address field on student.model

--- src/models/student.model.js
+++ src/models/student.model.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const studentSchema = new mongoose.Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
+  address: { type: String, required: true },
   curp: { type: String, unique: true, required: true },
   tutorId: {
     type: mongoose.Schema.Types.ObjectId,
